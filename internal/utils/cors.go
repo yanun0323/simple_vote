a4ecda6b0ca9ce51c24f3af5a39c3f8a9118ba6d
yanun0323 <a1256212562@gmail.com>
2024-07-31 06:31:35 +0800

    fix: cors

---
+++ internal/utils/cors.go
@@ -0,0 +1,15 @@
+package utils
+
+import "net/http"
+
+func CORS(fn func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		setCORS(w)
+		fn(w, r)
+	})
+}
+
+func setCORS(w http.ResponseWriter) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:8080")
+}
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 
 	"main/internal/controller/homepage"
 	"main/internal/controller/room"
+	"main/internal/utils"
 
 	"github.com/yanun0323/pkg/config"
 )
@@ -15,16 +16,16 @@ func main() {
 		slog.Error("config.Init", "err", err.Error())
 	}
 
-	http.HandleFunc("GET /vote", homepage.HomePage())
-	http.HandleFunc("GET /vote/{room_id}", room.GetRoom())
-	http.HandleFunc("GET /vote/{room_id}/{uid}", room.EnterRoom())
+	http.HandleFunc("GET /vote", utils.CORS(homepage.HomePage()))
+	http.HandleFunc("GET /vote/{room_id}", utils.CORS(room.GetRoom()))
+	http.HandleFunc("GET /vote/{room_id}/{uid}", utils.CORS(room.EnterRoom()))
 
-	http.HandleFunc("POST /api/vote/{room_id}", room.CreateRoom())
-	http.HandleFunc("POST /api/vote/{room_id}/{uid}", room.CreatePlayer())
+	http.HandleFunc("POST /api/vote/{room_id}", utils.CORS(room.CreateRoom()))
+	http.HandleFunc("POST /api/vote/{room_id}/{uid}", utils.CORS(room.CreatePlayer()))
 
 	// wss
-	http.HandleFunc("/api/vote/{room_id}/{uid}/player", room.ConnectPlayer())
-	http.HandleFunc("/api/vote/{room_id}/{uid}/host", room.ConnectHost())
+	http.HandleFunc("/api/vote/{room_id}/{uid}/player", utils.CORS(room.ConnectPlayer()))
+	http.HandleFunc("/api/vote/{room_id}/{uid}/host", utils.CORS(room.ConnectHost()))
 
 	// listen on port 8080
 	http.ListenAndServe(":8080", nil)
