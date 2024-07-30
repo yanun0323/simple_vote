11da40ff4bdb2edc1c6f6c72b4a671ccdd220529
yanun0323 <a1256212562@gmail.com>
2024-07-31 06:52:04 +0800

    fix: cors

--- internal/utils/cors.go
+++ internal/utils/cors.go
@@ -15,5 +15,5 @@ func SetCORS(w http.ResponseWriter) {
 	// w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, Connection")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding,Accept-Language, X-CSRF-Token, Authorization, Connection, Host, Origin, User-Agent, Cookie, Upgrade, Sec_Websocket_Extensions, Sec-WebSocket-Key, Sec-WebSocket-Version, Sec-WebSocket-Protocol, Sec-WebSocket-Version, Pragma, Cache-Control")
 }
